15552380271fde644b00e3357ca86fadf39862d5
knittmitens <gmartinezs2401@alumno.ipn.mx>
2026-03-02 23:43:32 -0600

    Mi primer hola mundo

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+//
+/*Java es un lebguaje de programacion multi paradigma que se adapta a las necesidades del modelo o arquitectura de programacion 
+java originalmente fue pensado para programar electrodomesticos, este lenguaje utiliza una maquina virtual*/
+class HolaMundo{
+    // el nombre de la clase debe ser el mismo que del archivo y no puede tener numeros al inicio 
+    //Este es el cuerpo de la clase 
+    //aqui se definen las variables globales 
+    public static void main(String[] args){
+        System.out.println("Hola Mundo Mundial");
+    }
+}
